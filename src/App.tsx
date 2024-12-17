f56e60e033809aa0ea42b4a3ab8a9c2e33cc232b
Patrick Prestes <patrickprestes977@gmail.com>
2024-12-17 15:17:14 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+function App() {
+  return (
+    <MantineProvider>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </MantineProvider>
+  );
+}
+
+export default App;
